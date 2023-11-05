0b6c4189edc91741c42e5be8e3d8ad2e736b3a46
Jhonatan <jhonatan.tamayom@udea.edu.co>
2023-11-04 21:42:09 -0500

    part 2 completed

--- part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
@@ -1,9 +1,39 @@
 package org.velezreyes.quiz.question6;
 
-public class VendingMachineImpl {
+public class VendingMachineImpl implements VendingMachine {
+
+  private int money;
 
   public static VendingMachine getInstance() {
-    // Fix me!
-    return null;
+    return new VendingMachineImpl();
+  }
+
+  @Override
+  public void insertQuarter() {
+    money += 25;
+  }
+
+  @Override
+  public Drink pressButton(String name) throws NotEnoughMoneyException, UnknownDrinkException {
+    switch (name) {
+      case "ScottCola":
+        if (money >= 75) {
+          money -= 75;
+          return new ScottCola("ScottCola", true);
+        } else {
+          throw new NotEnoughMoneyException();
+        }
+
+      case "KarenTea":
+        if (money >= 100) {
+          money -= 100;
+          return new KarenTea("KarenTea", false);
+        } else {
+          throw new NotEnoughMoneyException();
+        }
+
+      default:
+        throw new UnknownDrinkException();
+    }
   }
 }
